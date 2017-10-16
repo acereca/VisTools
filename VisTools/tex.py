3c6be93e0950874544590ce3a31dba207d9ef86d
acereca <tigerauge96@live.de>
2017-10-16 10:25:50 +0200

    add testcases, tex var script and pycharm compat to gitignore

--- VisTools/tex.py
+++ VisTools/tex.py
@@ -18,20 +18,42 @@ def df_tolatex(table: pd.DataFrame, texfile: str, formatterfunc = __form):
     table.to_latex(texfile, escape=False, formatters=[formatterfunc]*len(table.columns), index = False, encoding='utf-8')
 
 
-def unc_tolatex(value: unc.Variable, id: str,  texfile: str):
+def unc_tolatex(value: unc.core.Variable, identifier: str, texfile: str, name=''):
     """
-        creates a texfile containing a command containing the uncertainty value
+        creates or appends a .tex-file creating or updating a command containing the uncertainty value
     """
 
-    general1 = """\newcommand{\val}[2]{%
-        \IfEqCase{#1}{%"""
-    general2 = """}[\PackageError{unc_tolatex}{undefinded value: #1}{}]%
-    }"""
+    general1 = "\\newcommand{\\val}[2]{%\n\t\\IfEqCase{#1}{%\n"
+    general2 = "\t}[\\PackageError{unc_tolatex}{undefinded value: #1}{}]%\n}"
 
-    with open(texfile, 'r+') as f:
+    if name != '': name = name + " ="
+    newentry = "\t\t{{ {id} }}{{ {name} \\SI{{ {nom}+-{std} }}{{ {unit} }}}}\n".format(id=identifier, name=name, nom=value.n, std=value.s, unit="")
+
+    new = ""
+    #content = ""
+
+    with open(texfile, 'w+') as f:
         content = f.read()
+        #content = content[40:-57]
+        if content != "":
+            content = content.splitlines(1)[2:-2]
 
-        if content.startswith(general1) and content.endswith(general2):
-            vallines = content[len(general1):len(general2)]
+    with open(texfile, 'w') as f:
 
-        if 
+        done = False
+
+        for l in content:
+            if l.split()[1] == identifier:
+                new += newentry
+                done = True
+            else:
+                new += l
+
+        if not done:
+            new += newentry
+
+        f.write(general1 + new + general2)
+
+if __name__ == "__main__":
+    u = unc.ufloat(9,.4)
+    unc_tolatex(u, 'l_mu', '../testing/test_val.tex', '$\lambda_\mu$')
